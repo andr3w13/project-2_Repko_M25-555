7a9698d625141bca3e1dc53db931c1b6ff91cbfb
andr3w13 <petrarka895@gmail.com>
2025-11-02 17:46:31 +0300

    Создал файл даижка игры

---
+++ src/primitive_db/engine.py
@@ -0,0 +1,10 @@
+import prompt
+
+def welcome():
+    command = prompt.string('\n*** \n' +
+    '<command> exit - выйти из программы\n' +
+    '<command> help - справочная информация\n' +
+    'Введите команду: help\n' +
+    '\n<command> exit - выйти из программы\n' + 
+    '<command> help - справочная информация\n' + 
+    'Введите команду: _')
